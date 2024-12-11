52cc6fdf4a60438520c1278400f1daf15bd32287
fuu3048 <julian.agnew@willisisd.org>
2024-12-11 11:37:51 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-  challenge: false, // Set to true if you want to enable password protection.
+  challenge: true, // Set to true if you want to enable password protection.
   blocked: {
     // Normal users can ignore this option. This is for the owner of the website.
     "gointerstellar.app": "",
@@ -10,4 +10,4 @@ const config = {
   },
 };
 
-export default config;
+export default config;pnpm i && pnpm start
